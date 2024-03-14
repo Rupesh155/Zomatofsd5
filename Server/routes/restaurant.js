75cdaddc3b8b005e0dd9e75a1992e8decabd9ac8
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-03-14 14:15:05 +0530

    update

--- Server/routes/restaurant.js
+++ Server/routes/restaurant.js
@@ -37,12 +37,70 @@ router.get('/restro', async(req ,res)=>{
     }
 
 
+})
 
 
+router.get('/restro/:id', async(req,res)=>{
+    try{
+       let restaurant=    await  Restaurant.findById(req.params.id)
+       if(!restaurant){
+        return res.send('not found')
+       }
+       else{
+        return res.send(restaurant)
+       }
 
-
+    }
+    catch{
+        res.send('errr')
+    }
+      
 
 })
+
+
+
+
+router.patch('/restro/:id', async(req,res)=>{
+    try{
+       let restaurant=    await  Restaurant.findByIdAndUpdate(req.params.id, req.body, {new:true})
+       if(!restaurant){
+        return res.send('not found')
+       }
+       else{
+        return res.send(restaurant)
+       }
+
+    }
+    catch{
+        res.send('errr')
+    }
+      
+
+})
+
+
+router.delete('/restro/:id', async(req,res)=>{
+    try{
+       let restaurant=    await  Restaurant.findByIdAndDelete(req.params.id)
+       if(!restaurant){
+        return res.send('not found')
+       }
+       else{
+        return res.send('deletedddd')
+       }
+
+    }
+    catch{
+        res.send('errr')
+    }
+      
+
+})
+
+
+
+
 
 
 
